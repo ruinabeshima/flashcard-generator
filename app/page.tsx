370558bac295f8fb9be7d03c71d9007b1ca4a206
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-12-29 10:35:56 +0900

    Set up basic login and register routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <Link href="/login">Login</Link>
+      <Link href="/register">Register</Link>
+    </>
   );
 }
--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -51,11 +51,11 @@ export async function updateSession(request: NextRequest) {
     request.nextUrl.pathname !== "/" &&
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/register")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
-    url.pathname = "/auth/login";
+    url.pathname = "/login";
     return NextResponse.redirect(url);
   }
 
