852f919c003fcdc4e5623bfd8f181fa93c8d0b12
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-12-29 12:38:42 +0900

    Added loading state and error handling to Google OAuth Button

--- components/google-oauth-button.tsx
+++ components/google-oauth-button.tsx
@@ -3,6 +3,7 @@
 import { Button } from "./ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { createClient } from "@/lib/supabase/client";
+import { useState } from "react";
 
 interface GoogleOAuthButtonProps {
   className?: string;
@@ -14,27 +15,49 @@ export default function GoogleOAuthButton({
   className = "",
   purpose = "",
 }: GoogleOAuthButtonProps) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
   const handleClick = async () => {
-    const supabase = await createClient();
-    await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: {
-        redirectTo: `${location.origin}/auth/callback?next=/dashboard`,
-      },
-    });
+    try {
+      setError(null);
+      setLoading(true);
+      const supabase = createClient();
+      await supabase.auth.signInWithOAuth({
+        provider: "google",
+        options: {
+          redirectTo: `${location.origin}/auth/callback?next=/dashboard`,
+        },
+      });
+    } catch (error) {
+      setError(
+        error instanceof Error ? error.message : "An unknown error occurred"
+      );
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     // Google Login
-    <Button
-      type="button"
-      variant="outline"
-      size="lg"
-      className={`flex items-center gap-2 w-full ${className}`}
-      onClick={handleClick}
-    >
-      <FcGoogle className="h-4 w-4" aria-hidden="true" />
-      <span>Sign {purpose} with Google</span>
-    </Button>
+    <>
+      <Button
+        type="button"
+        variant="default"
+        size="lg"
+        className={`flex items-center gap-2 w-full ${className}`}
+        onClick={handleClick}
+        disabled={loading}
+      >
+        <FcGoogle className="h-4 w-4" aria-hidden="true" />
+        <span>
+          {error
+            ? error
+            : loading
+            ? "Loading..."
+            : `Sign ${purpose} with Google`}
+        </span>
+      </Button>
+    </>
   );
 }
