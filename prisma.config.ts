ceddf43eb3ebc8cb1898e079f6fd141a0d396050
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-01-21 10:59:20 +0900

    Handled type error in prisma config file

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,17 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const DATABASE_URL = process.env["DATABASE_URL"];
+if (!DATABASE_URL) {
+  throw new Error("Environment variable DATABASE_URL is not set");
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DIRECT_URL"],
+    url: DATABASE_URL,
   },
 });
