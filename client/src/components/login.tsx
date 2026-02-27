a0e3055997469c2c314f104e8e6384c76836c15a
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-02-27 12:50:17 +0900

    feat: add background image to Login and Register pages

--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -1,8 +1,14 @@
 import { SignIn } from "@clerk/clerk-react";
+import background from "../background.png";
 
 export default function Login() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat brightness-80 -z-10"
+      />
+
       <SignIn signUpUrl="/register" forceRedirectUrl="/dashboard" />
     </div>
   );
--- client/src/components/register.tsx
+++ client/src/components/register.tsx
@@ -1,8 +1,14 @@
 import { SignUp } from "@clerk/clerk-react";
+import background from "../background.png";
 
 export default function Register() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat brightness-80 -z-10"
+      />
+
       <SignUp signInUrl="/login" forceRedirectUrl="/dashboard" />
     </div>
   );
