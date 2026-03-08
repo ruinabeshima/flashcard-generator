b31c7a404cd387074fead14d6a941725d46e27e3
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-03-08 17:27:56 +0900

    feat: basic route setup for file upload using multer and r2 client

--- server/routes/resumes.ts
+++ server/routes/resumes.ts
@@ -1,9 +1,40 @@
 import express, { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { r2 } from "../lib/r2";
+import { upload } from "../lib/multer";
 import { requireAuth } from "@clerk/express";
 
 const resumeRouter = express.Router();
 
-resumeRouter.post("/upload", (req: Request, res: Response) => {});
+resumeRouter.post(
+  "/upload",
+  requireAuth(),
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    const { userId } = req.auth;
+    const { file } = req;
+
+    if (!file) {
+      res.status(400).json({ error: "No file uploaded" });
+      return;
+    }
+
+    try {
+      await r2.send(
+        new PutObjectCommand({
+          Bucket: process.env.R2_BUCKET_NAME,
+          Key: file.originalname,
+          Body: file.buffer,
+          ContentType: file.mimetype,
+        }),
+      );
+
+      res.status(201).json({ message: "File sent successfully" });
+    } catch {
+      res.status(500).json({ error: "Failed to upload file" });
+    }
+  },
+);
 
 export { resumeRouter };
